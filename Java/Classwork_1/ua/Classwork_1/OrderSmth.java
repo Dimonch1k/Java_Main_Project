b9a4d22a6604504829054432cfddd58af391d649
Dimonch1k <137522769+Dimonch1k@users.noreply.github.com>
2024-01-23 23:56:44 +0200

    Delete Java directory

--- Java/Classwork_1/ua/Classwork_1/OrderSmth.java
+++
@@ -1,6 +0,0 @@
-package ua.Classwork_1;
-
-public interface OrderSmth {
-    void orderFood(int food, double money);
-    void orderDrink(int drink, double money);
-}
--- Java/Classwork_1/ua/Classwork_1/ShowInformation.java
+++
@@ -1,6 +0,0 @@
-package ua.Classwork_1;
-
-public interface ShowInformation {
-    void showMenu();
-    void showDiscount(Customer customer);
-}
